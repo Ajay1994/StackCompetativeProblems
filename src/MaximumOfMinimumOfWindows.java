e5d0b4894292e330bcec47a6896f5127e0b4e288
pokemon <ajayjaiswalhi@gmail.com>
2016-05-26 16:11:55 +0530

    Initial COmmit2

--- src/MaximumOfMinimumOfWindows.java
+++ src/MaximumOfMinimumOfWindows.java
@@ -1,4 +1,19 @@
 import java.util.*;
+/*
+ * Given an integer array of size n, find the maximum of the minimum’s of every window size in the array.
+ *  Note that window size varies from 1 to n.
+
+	Example:
+	
+	Input:  arr[] = {10, 20, 30, 50, 10, 70, 30}
+	Output:         70, 30, 20, 10, 10, 10, 10
+	
+	First element in output indicates maximum of minimums of all 
+	windows of size 1.
+	Minimums of windows of size 1 are {10}, {20}, {30}, {50}, {10},
+	{70} and {30}.  Maximum of these minimums is 70
+	
+ */
 public class MaximumOfMinimumOfWindows {
 	private static int max(int i, int j) {
 		// TODO Auto-generated method stub
--- src/MaximumSizeRectangle.java
+++ src/MaximumSizeRectangle.java
@@ -1,4 +1,26 @@
+import java.util.*;
 
+/*
+ * Find the largest rectangular area possible in a given histogram where the largest rectangle can be 
+ * made of a number of contiguous bars. For simplicity, assume that all bars have same width and the width is 1 unit.
+
+	For example, consider the following histogram with 7 bars of heights {6, 2, 5, 4, 5, 2, 6}. 
+	The largest possible rectangle possible is 12
+	
+	
+	We traverse all bars from left to right, maintain a stack of bars. Every bar is pushed to stack once. A bar 
+	is popped from stack when a bar of smaller height is seen. When a bar is popped, we calculate the area with the 
+	popped bar as smallest bar. How do we get left and right indexes of the popped bar – the current index tells us 
+	the ‘right index’ and index of previous item in stack is the ‘left index’. Following is the complete algorithm.
+
+	1) Create an empty stack.
+	
+	2) Start from first bar, and do following for every bar ‘hist[i]’ where ‘i’ varies from 0 to n-1.
+	……a) If stack is empty or hist[i] is higher than the bar at top of stack, then push ‘i’ to stack.
+	……b) If this bar is smaller than the top of stack, then keep removing the top of stack while top of the stack is greater. Let the removed bar be hist[tp]. Calculate area of rectangle with hist[tp] as smallest bar. For hist[tp], the ‘left index’ is previous (previous to tp) item in stack and ‘right index’ is ‘i’ (current index).
+	
+	3) If the stack is not empty, then one by one remove all bars from stack and do step 2.b for every removed bar.
+ */
 public class MaximumSizeRectangle {
 
 	private static int max(int i, int j) {
